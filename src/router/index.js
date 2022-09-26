caad98e60289ed9f0e496e767b58d1c7892bfe33
Mitch <jamlets@protonmail.com>
2022-09-26 13:52:06 -0400

    marketplace

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Marketplace from '../views/Marketplace.vue'
+import NftDetails from '../views/NftDetails.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/Marketplace',
+    name: 'Marketplace',
+    component: Marketplace
+  },
+  {
+    path: '/NftDetails/:id',
+    name: 'NftDetails',
+    component: NftDetails,
+    props: true
+  },
+  
+
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+  scrollBehavior() {
+      document.getElementById('app').scrollIntoView({ behavior: 'smooth' });
+  }
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="content_grid home">
+    <div class="top_container">
+      <div class="top_content">
+        <h1>WELCOME</h1>
+        <p>CREATE - CONSERVE - NATUREFI</p>
+      </div>
+    </div>
+    <div class="login_container">
+      <Login/>
+    </div>
+    <div class="filter_container">
+      <Filter/>
+    </div>
+    <div class="grid_container">
+      <NftGrid :gridTitle="gridTitle"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Login from './components/Login.vue';
+import NftGrid from './components/NftGrid.vue';
+import Filter from './components/Filter.vue';
+import { ref } from 'vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Login,
+    NftGrid,
+    Filter
+  },
+  setup() {
+    const gridTitle = ref("User Collection")
+    return { gridTitle }
+  }
+
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
