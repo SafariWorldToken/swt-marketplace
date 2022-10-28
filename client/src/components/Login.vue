6ba198eafeaaa10beb8aea4cdfd572f2ccd0661c
Mitch <jamlets@protonmail.com>
2022-10-28 06:21:39 -0400

    check network etc.

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -26,24 +26,24 @@ export default {
       const setAccountAddress = (address) => store.commit('setAccountAddress', address)
       const setProvider = (provider) => store.commit('setProvider', provider)
 
-      const metaLogin = async () => {
-        try {
-          const provider = await $moralis.enableWeb3()
-          const chainId = await provider.chainId;
-          if ( chainId != 56 ) {
+      const checkNetwork = async (chainId) => {
+        if ( chainId != 56 ) {
               //store.state.modal["messageModal"]["message"] = "switching network...";
               //store.state.modal["messageModal"]["status"] = true;
               const newId = 56;
               await $moralis.switchNetwork(newId);
               //store.state.modal["messageModal"]["status"] = false;
-              const user = await $moralis.Web3.authenticate()
-              const address = user.get('ethAddress')
-              setUser(user)
-              setAccountAddress(address)
-              setProvider(provider)
-              //getUserNfts()
               return
           }
+        else {
+          console.log('network okay')
+        }
+      }
+      const metaLogin = async () => {
+        try {
+          const provider = await $moralis.enableWeb3()
+          const chainId = await provider.chainId;
+          checkNetwork(chainId)
           const user = await $moralis.Web3.authenticate()
           const address = user.get('ethAddress')
           setUser(user)
@@ -58,6 +58,8 @@ export default {
       }
       const handleCurrentUser = async () => {
           const provider = await $moralis.enableWeb3()
+          const chainId = await provider.chainId;
+          checkNetwork(chainId)
           const user = $moralis.User.current()
           if (user) {
             const address = user.get('ethAddress')
